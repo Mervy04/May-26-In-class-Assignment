10183d57ee9a39ee42e76449232d2a7d24ad46b5
Mervy04 <87144944+Mervy04@users.noreply.github.com>
2022-05-27 10:10:34 -0400

    Create SomeDataClass.java

---
+++ SomeDataClass.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class SomeDataClass {
+    private final static String seperator = "\n===========================================================================\n";
+
+    // data fields
+    private String SecondData;
+    private int FirstData;
+    private boolean IsFirstSecondData;
+
+
+    // public methods
+    public void addFirstData(int FirstData) { // public method to add the First data to list or array
+
+        System.out.println(FirstData);
+    }
+
+
+    public void addSecondData(String SecondData) { // public method to add the Second data to list or array
+
+        System.out.println(SecondData);
+    }
+
+
+    public void addIsFirstSecondData(String IsFirstSecondData) { // public method to add the IsFirstSecond data to list or array
+
+        System.out.println(IsFirstSecondData);
+    }
+
+}
