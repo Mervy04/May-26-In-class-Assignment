70b3ce58ae3eb2ca98c00358f2e165068dcfce57
Mervy04 <87144944+Mervy04@users.noreply.github.com>
2022-05-27 10:10:05 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.ArrayList; // import the ArrayList class
+public class Main {
+    ArrayList<String> SecondData = new ArrayList<String>(); // Create an ArrayList String object
+    ArrayList<Integer> FirstData = new ArrayList<>(); // Create an ArrayList integer object
+    ArrayList<Integer> IsFirstSecondData = new ArrayList<>(); // Create an ArrayList boolean object
+    public static void main(String[] args) {
+
+        int first = 1;
+        String second = "Second";
+        boolean IsFirstSecond = true;
+
+
+        https://www.w3schools.com/java/java_arraylist.asp
+
+    }
+}
